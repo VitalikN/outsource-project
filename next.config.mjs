9368b6a876e950578102f81ffcda998729400e4c
vitalik.nozhenko@gmail.com <109283146+VitalikN@users.noreply.github.com>
2024-01-23 20:33:58 +0200

    fix nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  output: "export",
-
-  images: {
-    unoptimized: true,
-  },
-  trailingSlash: true,
-};
+const nextConfig = {};
 
 export default nextConfig;
